e3d402a4a153d492973bb5784f98c3d4f8136140
Shinobi325 <man849916@gmail.com>
2022-11-17 12:42:18 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	operadores/Cargo.lock
    #	operadores/Cargo.toml
    
    # Añadido
    Arreglos/Arrays
    
    # Añadido
    Uso de Tuplas en Rust

--- return_en_rust/src/main.rs
+++ return_en_rust/src/main.rs
@@ -1,11 +1,19 @@
 // Return
 
+// Si tenemos una función la cual por ciertos motivos necesita
+// finalizar y retornar un valor antes de tiempo podemos utilizar
+// la palabra reservada "Return"
+
+// Ejemplo
+
 // Definimos una función y le asignamos sus parámetros
 fn factorial(numero: u32) -> u32 {
     if numero == 1 {
-        numero; // Si nuestro parámetro se encuentra en la ultima linea del bloque
+        // Si nuestro parámetro se encuentra en la ultima linea del bloque
         // podemos ahorrarnos el uso de return
-    } else {
+         numero // Si nuestro parámetro se encuentra en la ultima linea del bloque
+        // podemos ahorrarnos el uso de return
+    } else { // no finalizaremos numero con punto y coma ";"
         factorial(numero - 1) * numero
     }
 }
